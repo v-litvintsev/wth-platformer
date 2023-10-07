1cf7d4ebc3792de3e3fcc44ace4fb2f651a42cdb
Vladimir Litvintsev <vl.litvintsev@yandex.ru>
2023-10-07 19:23:55 +0300

    fix

--- server/src/ws-handlers/index.ts
+++ server/src/ws-handlers/index.ts
@@ -1,7 +1,4 @@
 import WebSocket, { Server } from 'ws'
-import ChatHandler from './chat-handler'
-import HomeHandler from './home-handler'
-import OnlineUsersHandler from './online-users-handler'
 import { TWsMessage } from './types'
 
 export default (wss: Server) => (ws: WebSocket) => {
