8b4d6af57c7f6f889e9cf658d8e2082194191e8e
Ilya <sia.ilya.dev@gmail.com>
2023-10-07 21:59:36 +0300

    feature: вернул ip для WS

--- client/src/http-client/constants.ts
+++ client/src/http-client/constants.ts
@@ -1,2 +1,2 @@
-export const WS_SERVER_ADDRESS = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/ws' : 'wss://haha.flint3s.ru/ws'
-export const SERVER_ADDRESS = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://haha.flint3s.ru'
+export const WS_SERVER_ADDRESS = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/ws' : 'ws://217.18.63.205:3001/ws'
+export const SERVER_ADDRESS = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://217.18.63.205:3001'
