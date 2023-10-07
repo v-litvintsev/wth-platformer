bcba00557d9cd45345b7a2cb87cc9eb2390baaf1
Ilya <sia.ilya.dev@gmail.com>
2023-10-07 18:45:16 +0300

    feature: Экран управления, хэш роутер

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.min.css'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import AppRouter from './AppRouter'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import './styles/globals.scss'
@@ -8,10 +8,10 @@ import './styles/globals.scss'
 export const queryClient = new QueryClient()
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <AppRouter />
     </QueryClientProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 )
